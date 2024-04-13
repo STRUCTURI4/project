7c37b4e8b01b2090b3dd9d4895eb76c043d24af8
Alexey <koldin.1997@mail.ru>
2024-04-13 11:34:14 +0300

    создали первую задачу в уроке два

---
+++ task01/Program.cs
@@ -0,0 +1,11 @@
+﻿int [] rew = new int [5];
+rew[0] = 5;
+rew[1] = 23;
+rew[2] = 67;
+rew[3] = 99;
+rew[4] = 7;
+Console.WriteLine(rew[0]);
+Console.WriteLine(rew[1]);
+Console.WriteLine(rew[2]);
+Console.WriteLine(rew[3]);
+Console.WriteLine(rew[4]);
